ff9dcae76873b1f7fa0b4e758ebd0eee99330c47
ggzor <30713864+ggzor@users.noreply.github.com>
2017-08-25 22:10:52 -0500

    [#1] Agregando archivos de proyecto y pruebas unitarias para AguaSB.Extensiones

---
+++ AguaSB.Extensiones.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AguaSB.Extensiones.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("AguaSB.Extensiones.Tests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca33cdc8-6bc6-4374-965c-f2221e309f06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
