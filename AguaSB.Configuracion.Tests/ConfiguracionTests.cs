92b407ee4fc93a30cdf3d45ed95941b63b0b8657
ggzor <30713864+ggzor@users.noreply.github.com>
2017-08-28 14:23:55 -0500

    Completando API de Configuracion

---
+++ AguaSB.Configuracion.Tests/ConfiguracionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AguaSB.Configuracion.Tests
+{
+    [TestFixture]
+    public class ConfiguracionTests
+    {
+        [TestCase("tmp", typeof(string), ".json", "tmp/String.json")]
+        [TestCase("tmp/", typeof(string), ".json", "tmp/String.json")]
+        [TestCase("tmp\\", typeof(string), ".json", "tmp/String.json")]
+        [TestCase("", typeof(string), ".json", "String.json")]
+        [TestCase("\\tmp/", typeof(string), ".json", "tmp/String.json")]
+        public void Combinar_Deberia_CombinarCorrectamente(string subdirectorio, Type tipo, string extension, string resultado)
+        {
+            var resultadoCombinacion = Configuracion.Combinar(subdirectorio, tipo, extension);
+
+            Assert.AreEqual(resultado, resultadoCombinacion);
+        }
+    }
+}
---
+++ AguaSB.Configuracion/Configuracion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace AguaSB.Configuracion
+{
+    /// <summary>
+    /// Utilerías para cargar configuraciones con facilidad. Es básicamente un adaptador para JSON.
+    /// </summary>
+    public static class Configuracion
+    {
+
+        public const string SubdirectorioDeConfiguracion = "Configuracion";
+
+        public const string ExtensionDeArchivos = ".json";
+
+        public static T Cargar<T>(FileInfo direccion)
+        {
+            string ExtraerTexto()
+            {
+                using (var stream = direccion.OpenText())
+                    return stream.ReadToEnd();
+            }
+
+            return JsonConvert.DeserializeObject<T>(ExtraerTexto());
+        }
+
+        /// <summary>
+        /// Carga el archivo "<paramref name="subdirectorio"/>/<typeparamref name="T"/>.<paramref name="extension"/>"
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="subdirectorio"></param>
+        /// <returns></returns>
+        public static T Cargar<T>(string subdirectorio = SubdirectorioDeConfiguracion, string extension = ".json") =>
+            Cargar<T>(new FileInfo(Combinar(subdirectorio, typeof(T), extension)));
+
+
+        public static string Combinar(string subdirectorio, Type tipo, string extension)
+        {
+            string Simplificar(string s) => s.Trim('/', '\\');
+
+            return Simplificar(Simplificar(subdirectorio) + "/" + tipo.Name + extension);
+        }
+
+
+        public static void Guardar(object objeto, FileInfo direccion)
+        {
+            var texto = JsonConvert.SerializeObject(objeto);
+
+            using (var stream = new StreamWriter(direccion.Open(FileMode.Create)))
+                stream.Write(texto);
+        }
+
+        public static void Guardar(object objeto, string subdirectorio = SubdirectorioDeConfiguracion, string extension = ExtensionDeArchivos) =>
+            Guardar(objeto, new FileInfo(Combinar(subdirectorio, objeto.GetType(), extension)));
+    }
+}
