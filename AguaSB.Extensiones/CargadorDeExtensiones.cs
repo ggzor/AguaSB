05f7fe85982e19bb0b767fdd50788b0cbe881184
ggzor <30713864+ggzor@users.noreply.github.com>
2017-08-29 17:00:00 -0500

    [#1] Actualizando API de Extensiones

---
+++ AguaSB.Extensiones/CargadorDeExtensiones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AguaSB.Extensiones
+{
+    /// <summary>
+    /// Carga archivos *.dll especificados que contengan la clase especificada.
+    /// </summary>
+    public class CargadorDeExtensiones
+    {
+
+        public IEnumerable<T> Cargar<T>(FileInfo archivo) where T : class
+        {
+            if (archivo == null)
+                throw new ArgumentNullException(nameof(archivo));
+
+            var ensamblado = Assembly.LoadFile(archivo.FullName);
+
+            return from tipo in ensamblado.GetTypes()
+                   where tipo.IsSubclassOf(typeof(T))
+                   select Activator.CreateInstance(tipo) as T;
+        }
+    }
+}
