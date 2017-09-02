72e99c4367290a445be47cf53ae41d10bb663cb9
ggzor <30713864+ggzor@users.noreply.github.com>
2017-09-01 21:16:05 -0500

    Agregando paquetes de tests y pruebas unitarias

---
+++ AguaSB.Navegacion.Tests/NodosTests.cs
@@ -0,0 +1,38 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AguaSB.Navegacion.Tests
+{
+    [TestFixture]
+    public class NodosTests
+    {
+
+        private static readonly Fixture Cualquiera = new Fixture();
+
+        [Test]
+        public async Task DeberiaLlamar_Inicializacion_CuandoSeLlamaEntrar()
+        {
+            var llamado = false;
+            var nodo = new NodoHoja()
+            {
+                Inicializacion = () =>
+                {
+                    llamado = true;
+                    return Task.CompletedTask;
+                }
+            };
+            var colaNavegacion = Cualquiera.Create<ColaNavegacion>();
+
+            await nodo.Entrar(colaNavegacion);
+
+            Assert.True(llamado);
+        }
+
+    }
+}
