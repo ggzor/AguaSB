270ee166a76492d0a3801cfc9b13e754f40357c9
ggzor <30713864+ggzor@users.noreply.github.com>
2017-09-16 11:28:00 -0500

    Agregando proyectos para las extensiones

---
+++ AguaSB.Usuarios.ViewModels/Agregar.cs
@@ -0,0 +1,34 @@
+﻿using GGUtils.MVVM.Async;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Waf.Foundation;
+
+namespace AguaSB.Usuarios.ViewModels
+{
+    public class Agregar : ValidatableModel
+    {
+        #region Comandos
+
+        public AsyncDelegateCommand<int> AgregarComando { get; }
+
+        #endregion
+
+        public Agregar()
+        {
+            AgregarComando = new AsyncDelegateCommand<int>(AgregarUsuario, PuedeAgregarUsuario);
+        }
+
+        private bool PuedeAgregarUsuario()
+        {
+            return true;
+        }
+
+        private Task<int> AgregarUsuario()
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
