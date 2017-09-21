6ee5930d13c08e09f7314b6191d7cfebb70fc978
ggzor <30713864+ggzor@users.noreply.github.com>
2017-09-20 21:27:34 -0500

    Agregando colores de aplicación

---
+++ AguaSB.Estilos/Color.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WPF = System.Windows.Media;
+
+namespace AguaSB.Estilos
+{
+    public class Color
+    {
+        public string Nombre { get; }
+
+        public string NombreMahApps { get; }
+
+        public WPF.Color ColorWPF { get; }
+
+        public WPF.Brush BrochaWPF { get; }
+
+        public Color(string nombre, string nombreMahApps, WPF.Color colorWPF)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+                throw new ArgumentNullException(nameof(nombre));
+
+            if (string.IsNullOrWhiteSpace(nombreMahApps))
+                throw new ArgumentNullException(nameof(nombreMahApps));
+
+            Nombre = nombre;
+            NombreMahApps = nombreMahApps;
+            ColorWPF = colorWPF;
+            BrochaWPF = new WPF.SolidColorBrush(colorWPF);
+
+            BrochaWPF.Freeze();
+        }
+    }
+}
---
+++ AguaSB.Estilos/Colores.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WPF = System.Windows.Media;
+
+using MoreLinq;
+
+namespace AguaSB.Estilos
+{
+    public static class Colores
+    {
+        #region Calcular
+        public static WPF.Color ColorDe(string colorCadena)
+        {
+            if (colorCadena == null)
+                throw new ArgumentNullException(nameof(colorCadena));
+
+            colorCadena = colorCadena.ToLower();
+            return WPF.Color.FromArgb(Int(colorCadena.Take(2)), Int(colorCadena.Slice(2, 2)), Int(colorCadena.Slice(4, 2)), Int(colorCadena.TakeLast(2)));
+        }
+
+        private static IEnumerable<KeyValuePair<char, byte>> Ajustar(int cantidad, char inicio, int inicioN) =>
+            Enumerable.Range(0, cantidad)
+            .Select(i => new KeyValuePair<char, byte>((char)(inicio + i), (byte)(i + inicioN)));
+
+        private static readonly Dictionary<char, byte> Byte = Ajustar(10, '0', 0).Concat(Ajustar(6, 'a', 10)).ToDictionary(kv => kv.Key, kv => kv.Value);
+
+        private static byte Int(IEnumerable<char> enumerable) => enumerable.Fold((c1, c2) => (byte)((Byte[c1] << 4) + Byte[c2]));
+        #endregion
+
+        public static Color CalcularColor(string nombre, string codigoHex, string nombreMahApps) =>
+            new Color(nombre, nombreMahApps, ColorDe(codigoHex));
+
+        public static readonly Color Azul = CalcularColor(nameof(Azul), "CC119EDA", "Blue");
+        public static readonly Color Naranja = CalcularColor(nameof(Naranja), "CCFA6800", "Orange");
+        public static readonly Color Verde = CalcularColor(nameof(Verde), "CC60A917", "Green");
+        public static readonly Color Acero = CalcularColor(nameof(Acero), "CC647687", "Steel");
+        public static readonly Color Cobalto = CalcularColor(nameof(Cobalto), "CC0050EF", "Cobalt");
+        public static readonly Color Purpura = CalcularColor(nameof(Purpura), "CC6459DF", "Purple");
+        public static readonly Color Cafe = CalcularColor(nameof(Cafe), "CC825A2C", "Brown");
+        public static readonly Color Rojo = CalcularColor(nameof(Rojo), "CCE51400", "Red");
+        public static readonly Color Rosa = CalcularColor(nameof(Rosa), "CCF472D0", "Pink");
+        public static readonly Color Crimson = CalcularColor(nameof(Crimson), "CCA20025", "Crimson");
+    }
+}
