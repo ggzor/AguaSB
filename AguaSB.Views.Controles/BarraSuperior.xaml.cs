2481bd373ad2fd3c99e5dde9a4977032676133cb
ggzor <30713864+ggzor@users.noreply.github.com>
2017-09-25 17:52:54 -0500

    Agregando proyecto de controles personalizados para la solución

---
+++ AguaSB.Views.Controles/BarraSuperior.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AguaSB.Views.Controles
+{
+    public partial class BarraSuperior : UserControl
+    {
+        public FrameworkElement Icono
+        {
+            get { return (FrameworkElement)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icono), typeof(FrameworkElement), typeof(BarraSuperior), new PropertyMetadata(null));
+
+
+        public string Titulo
+        {
+            get { return (string)GetValue(TituloProperty); }
+            set { SetValue(TituloProperty, value); }
+        }
+
+        public static readonly DependencyProperty TituloProperty =
+            DependencyProperty.Register(nameof(Titulo), typeof(string), typeof(BarraSuperior), new PropertyMetadata("Titulo"));
+
+
+        public BarraSuperior()
+        {
+            InitializeComponent();
+        }
+    }
+}
