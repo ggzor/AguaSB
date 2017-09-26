24caa1a16e72894a403ee924ab044a67a841316b
ggzor <30713864+ggzor@users.noreply.github.com>
2017-09-26 15:56:05 -0500

    Agregando entidad Negocio al Nucleo

---
+++ AguaSB.Nucleo/Negocio.cs
@@ -0,0 +1,26 @@
+﻿using AguaSB.Nucleo.Mensajes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AguaSB.Nucleo
+{
+    [Table("Negocios")]
+    public class Negocio : Usuario
+    {
+        private string nombre;
+        private string rfc;
+
+        [Required(ErrorMessage = Validacion.CampoRequerido)]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { N.Validate(ref nombre, value); }
+        }
+
+        public string Rfc
+        {
+            get { return rfc; }
+            set { N.Validate(ref rfc, value); }
+        }
+    }
+}
