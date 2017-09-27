1d498db3295f4c004f38e18a3457bf06a3429d6a
ggzor <30713864+ggzor@users.noreply.github.com>
2017-09-27 11:47:58 -0500

    Agregando extensión para hacer observables los notificantes

---
+++ AguaSB.Utilerias/NotifyPropertyChangedEx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace AguaSB.Utilerias
+{
+    public static class NotifyPropertyChangedEx
+    {
+        public static IObservable<(object Source, PropertyChangedEventArgs Args)> ToObservableProperties(this INotifyPropertyChanged notifier)
+        {
+            return Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                handler => notifier.PropertyChanged += handler,
+                handler => notifier.PropertyChanged -= handler)
+                .Select(eventPattern => (eventPattern.Sender, eventPattern.EventArgs));
+        }
+    }
+}
