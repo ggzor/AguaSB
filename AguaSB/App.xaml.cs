943658a5d882b4f9026607134029d09c6b38278f
ggzor <30713864+ggzor@users.noreply.github.com>
2017-10-02 12:28:37 -0500

    Agregando el paquete CastleWindsor

--- AguaSB/App.xaml.cs
+++ AguaSB/App.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AguaSB
 {
-    /// <summary>
-    /// Lógica de interacción para App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
