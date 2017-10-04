dba78fd016981ea54037d25b9b256794d0e8c8e6
ggzor <30713864+ggzor@users.noreply.github.com>
2017-10-03 19:42:30 -0500

    Refactorizando ajuste de tamaños

---
+++ AguaSB.Views/Utilerias/AjustadorTamanoObjetos.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AguaSB.Views.Utilerias
+{
+    /// <summary>
+    /// Calcula el ancho de un objeto que se ajusta a un contenedor del tamaño especificado.
+    /// Su principal aplicación es en la clase <see cref="System.Windows.Controls.WrapPanel"/>
+    /// </summary>
+    public class AjustadorTamanoObjetos : INotifyPropertyChanged
+    {
+        #region Logica INotify
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName]string prop = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        #endregion
+
+        #region Campos y propiedades
+        private double anchoContenedor;
+        private double margen;
+        private double anchoObjetoMinimo;
+        private double anchoObjeto;
+
+        public double AnchoContenedor
+        {
+            get { return anchoContenedor; }
+            set
+            {
+                anchoContenedor = value;
+                RecalcularTamanoObjeto();
+            }
+        }
+
+        public double Margen
+        {
+            get { return margen; }
+            set
+            {
+                margen = value;
+                RecalcularTamanoObjeto();
+            }
+        }
+
+        public double AnchoObjetoMinimo
+        {
+            get { return anchoObjetoMinimo; }
+            set
+            {
+                anchoObjetoMinimo = value;
+                RecalcularTamanoObjeto();
+            }
+        }
+
+        public double AnchoObjeto
+        {
+            get { return anchoObjeto; }
+            private set
+            {
+                anchoObjeto = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        private void RecalcularTamanoObjeto()
+        {
+            var anchoRequerido = AnchoObjetoMinimo + (2 * Margen);
+
+            if (AnchoContenedor < anchoRequerido)
+            {
+                // Sólo cabe un objeto, lo compactamos de acuerdo al contenedor;
+                AnchoObjeto = AnchoContenedor - (2 * Margen);
+            }
+            else
+            {
+                var cantidadDeObjetos = (int)(AnchoContenedor / anchoRequerido);
+
+                var espacioPorObjeto = AnchoContenedor / cantidadDeObjetos;
+
+                AnchoObjeto = espacioPorObjeto - (2 * Margen);
+            }
+        }
+    }
+}
--- AguaSB/VentanaPrincipal.xaml.cs
+++ AguaSB/VentanaPrincipal.xaml.cs
@@ -3,6 +3,8 @@ using System.Windows;
 
 using MahApps.Metro.Controls;
 
+using AguaSB.Views.Utilerias;
+
 namespace AguaSB
 {
     public partial class VentanaPrincipal : MetroWindow
@@ -12,43 +14,25 @@ namespace AguaSB
         public VentanaPrincipal(VentanaPrincipalViewModel viewModel)
         {
             DataContext = ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+
+            Ajustador.AnchoObjetoMinimo = 400.0;
+            Ajustador.Margen = 15.0;
+
             InitializeComponent();
         }
 
-        #region Propiedades
-        public double AnchoExtensionView
+        #region Ajuste de tamaños
+        public AjustadorTamanoObjetos Ajustador
         {
-            get { return (double)GetValue(AnchoExtensionViewProperty); }
-            set { SetValue(AnchoExtensionViewProperty, value); }
-        }
-        #endregion
-
-        private const double AnchoMinimoPorView = 400.0;
-        public const double Margen = 20.0;
-        public const double AnchoRequerido = AnchoMinimoPorView + (2 * Margen);
-
-        private void AjustarExtensionViews(object sender, SizeChangedEventArgs e)
-        {
-            var anchoContenedor = e.NewSize.Width;
-
-            if (anchoContenedor < AnchoRequerido)
-            {
-                // Sólo cabe un view, lo compactamos de acuerdo al contenedor;
-                AnchoExtensionView = anchoContenedor - (2 * Margen);
-            }
-            else
-            {
-                var cantidadDeViews = (int)(anchoContenedor / AnchoRequerido);
-
-                var espacioPorView = anchoContenedor / cantidadDeViews;
-
-                AnchoExtensionView = espacioPorView - (2 * Margen);
-            }
+            get { return (AjustadorTamanoObjetos)GetValue(AjustadorProperty); }
+            set { SetValue(AjustadorProperty, value); }
         }
 
-        #region DP´s
-        public static readonly DependencyProperty AnchoExtensionViewProperty =
-            DependencyProperty.Register(nameof(AnchoExtensionView), typeof(double), typeof(VentanaPrincipal), new PropertyMetadata(400.0));
+        public static readonly DependencyProperty AjustadorProperty =
+            DependencyProperty.Register(nameof(Ajustador), typeof(AjustadorTamanoObjetos), typeof(VentanaPrincipal), new PropertyMetadata(new AjustadorTamanoObjetos()));
+
+        private void AjustarAnchos(object sender, SizeChangedEventArgs e) =>
+            Ajustador.AnchoContenedor = e.NewSize.Width;
         #endregion
     }
 }
