e726aee9b09b03b757d00e91121c732de69ed45d
ggzor <30713864+ggzor@users.noreply.github.com>
2017-10-13 22:20:58 -0500

    Agregando soporte para iconos laterales

---
+++ AguaSB.Estilos/Icono.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace AguaSB.Estilos
+{
+    public static class Icono
+    {
+        public static readonly DependencyProperty EnfocadoProperty =
+            DependencyProperty.RegisterAttached("Enfocado", typeof(bool), typeof(Icono), new PropertyMetadata(false));
+
+        public static void SetEnfocado(UIElement elem, bool valor) => elem.SetValue(EnfocadoProperty, valor);
+
+        public static bool GetEnfocado(UIElement elem) => (bool)elem.GetValue(EnfocadoProperty);
+
+
+        public static readonly DependencyProperty ErrorProperty =
+            DependencyProperty.RegisterAttached("Error", typeof(bool), typeof(Icono), new PropertyMetadata(false));
+
+        public static void SetError(UIElement elem, bool valor) => elem.SetValue(ErrorProperty, valor);
+
+        public static bool GetError(UIElement elem) => (bool)elem.GetValue(ErrorProperty);
+
+    }
+}
