924f5829ca062e630c80f24e50471711819bb2f4
ggzor <30713864+ggzor@users.noreply.github.com>
2017-10-16 14:22:39 -0500

    Agregando enlaces a contrato

--- AguaSB.Contratos.Views/Agregar.xaml.cs
+++ AguaSB.Contratos.Views/Agregar.xaml.cs
@@ -2,20 +2,28 @@
 using System.Windows.Controls;
 
 using AguaSB.Views;
+using AguaSB.Navegacion;
 
 namespace AguaSB.Contratos.Views
 {
     public partial class Agregar : UserControl, IView
     {
+        public INavegador Navegador { get; }
+
         public ViewModels.Agregar ViewModel { get; }
 
-        public Agregar(ViewModels.Agregar viewModel)
+        public Agregar(ViewModels.Agregar viewModel, INavegador navegador)
         {
+            Navegador = navegador ?? throw new ArgumentNullException(nameof(navegador));
             DataContext = ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
 
             InitializeComponent();
 
             ViewModel.Enfocar += (_, __) => TipoContrato.Focus();
         }
+
+        private void AgregarUsuario_Click(object sender, System.Windows.RoutedEventArgs e) => Navegador.Navegar("Usuarios/Agregar", null);
+
+        private void BuscarUsuario_Click(object sender, System.Windows.RoutedEventArgs e) => Navegador.Navegar("Usuarios/Listado", null);
     }
 }
