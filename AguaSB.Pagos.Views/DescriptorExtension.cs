da8cf8fa202da2200a748bc839a2949208cd6c39
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-03 16:50:53 -0600

    Agregando módulo de Pagos

---
+++ AguaSB.Pagos.Views/DescriptorExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+using MahApps.Metro.IconPacks;
+
+using AguaSB.Estilos;
+using AguaSB.Extensiones;
+
+namespace AguaSB.Pagos.Views
+{
+    public class DescriptorExtension : IExtension
+    {
+        public DescriptorExtension()
+        {
+        }
+
+        public string Nombre => nameof(Pagos);
+
+        public string Version => "v0.1.0";
+
+        public string Descripcion =>
+            "Administrar y realizar pagos de los contratos en el sistema.";
+
+        public FrameworkElement Icono { get; } = new PackIconModern()
+        {
+            Kind = PackIconModernKind.Money,
+            Foreground = Brushes.White
+        };
+
+        public Tema Tema { get; } = Temas.Verde;
+
+        public IEnumerable<Operacion> Operaciones => new Operacion[]
+        {
+        };
+    }
+}
