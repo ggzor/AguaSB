45f3be6bcf0b5b722246e804cac5ac64b137b4d6
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-05 17:18:06 -0600

    [#17] Refactorizando Agregar de Usuarios

---
+++ AguaSB.Usuarios.Views/Controles/ContactosView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+using AguaSB.Nucleo;
+
+namespace AguaSB.Usuarios.Views.Controles
+{
+    public partial class ContactosView : UserControl
+    {
+        public ContactosView()
+        {
+            InitializeComponent();
+            Reestablecer();
+        }
+
+        public void Reestablecer() => Contactos = new List<Contacto>();
+
+        public IEnumerable<TipoContacto> TiposContacto
+        {
+            get { return (IEnumerable<TipoContacto>)GetValue(TiposContactoProperty); }
+            set { SetValue(TiposContactoProperty, value); }
+        }
+
+        public IList<Contacto> Contactos
+        {
+            get { return (IList<Contacto>)GetValue(ContactosProperty); }
+            set { SetValue(ContactosProperty, value); }
+        }
+
+        public static readonly DependencyProperty TiposContactoProperty =
+            DependencyProperty.Register(nameof(TiposContacto), typeof(IEnumerable<TipoContacto>), typeof(ContactosView), new PropertyMetadata(Enumerable.Empty<TipoContacto>()));
+
+        public static readonly DependencyProperty ContactosProperty =
+            DependencyProperty.Register(nameof(Contactos), typeof(IList<Contacto>), typeof(ContactosView), new PropertyMetadata(null));
+    }
+}
---
+++ AguaSB.Usuarios.Views/Controles/PersonaView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+using AguaSB.Nucleo;
+
+namespace AguaSB.Usuarios.Views.Controles
+{
+    public partial class PersonaView : UserControl
+    {
+        public PersonaView()
+        {
+            InitializeComponent();
+        }
+
+        #region Props
+        public Persona Persona
+        {
+            get { return (Persona)GetValue(PersonaProperty); }
+            set { SetValue(PersonaProperty, value); }
+        }
+
+        public IEnumerable<string> SugerenciasNombres
+        {
+            get { return (IEnumerable<string>)GetValue(SugerenciasNombresProperty); }
+            set { SetValue(SugerenciasNombresProperty, value); }
+        }
+
+        public IEnumerable<string> SugerenciasApellidos
+        {
+            get { return (IEnumerable<string>)GetValue(SugerenciasApellidosProperty); }
+            set { SetValue(SugerenciasApellidosProperty, value); }
+        }
+        #endregion
+
+        #region DP´s
+        public static readonly DependencyProperty PersonaProperty =
+            DependencyProperty.Register(nameof(Persona), typeof(Persona), typeof(PersonaView), new PropertyMetadata(new Persona()));
+
+        public static readonly DependencyProperty SugerenciasNombresProperty =
+            DependencyProperty.Register(nameof(SugerenciasNombres), typeof(IEnumerable<string>), typeof(PersonaView), new PropertyMetadata(Enumerable.Empty<string>()));
+
+        public static readonly DependencyProperty SugerenciasApellidosProperty =
+            DependencyProperty.Register(nameof(SugerenciasApellidos), typeof(IEnumerable<string>), typeof(PersonaView), new PropertyMetadata(Enumerable.Empty<string>()));
+        #endregion
+    }
+}
