47efcb323fc77c52453d60dac5605b9058c59c64
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-05 19:41:03 -0600

    Agregando NegocioView

---
+++ AguaSB.Usuarios.Views/Controles/NegocioView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using AguaSB.Nucleo;
+
+namespace AguaSB.Usuarios.Views.Controles
+{
+    public partial class NegocioView : UserControl
+    {
+        public NegocioView()
+        {
+            InitializeComponent();
+        }
+
+        public void Enfocar() => Nombre.Focus();
+
+        public Negocio Negocio
+        {
+            get { return (Negocio)GetValue(NegocioProperty); }
+            set { SetValue(NegocioProperty, value); }
+        }
+
+        public static readonly DependencyProperty NegocioProperty =
+            DependencyProperty.Register(nameof(Negocio), typeof(Negocio), typeof(NegocioView), new PropertyMetadata(new Negocio()));
+    }
+}
