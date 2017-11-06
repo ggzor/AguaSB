24b22ae04f0f7f47c435035d37d998debb2cea62
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-05 21:33:19 -0600

    Agregando CallbackConverter

---
+++ AguaSB.Views/Utilerias/CallbackConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AguaSB.Views.Utilerias
+{
+    public class CallbackConverter : IValueConverter
+    {
+        public Func<object, object> Callback { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            Callback?.Invoke(value) ?? DependencyProperty.UnsetValue;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new InvalidOperationException();
+    }
+}
