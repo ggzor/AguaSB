dcfe5961448fc78867f35fcbb3e860ce347ae6a1
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-07 14:10:18 -0600

    Agregando inversor de propiedad booleana

---
+++ AguaSB.Views/Utilerias/InvertBoolConverter.cs
@@ -0,0 +1,9 @@
+﻿namespace AguaSB.Views.Utilerias
+{
+    public class InvertBoolConverter : FuncValueConverter
+    {
+        public InvertBoolConverter() : base(VerificarValor) { }
+
+        private static object VerificarValor(object arg) => !((arg as bool?) == true);
+    }
+}
