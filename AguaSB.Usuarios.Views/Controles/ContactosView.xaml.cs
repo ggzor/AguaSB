9b952871f70f9008f0c364eb29d7049aaaaf53f1
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-06 19:21:43 -0600

    [#17] Agregando ContactosView

--- AguaSB.Usuarios.Views/Controles/ContactosView.xaml.cs
+++ AguaSB.Usuarios.Views/Controles/ContactosView.xaml.cs
@@ -1,21 +1,118 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
+using MahApps.Metro.IconPacks;
+
+using AguaSB.Estilos;
 using AguaSB.Nucleo;
+using AguaSB.Views.Utilerias;
+using System.Waf.Applications;
 
 namespace AguaSB.Usuarios.Views.Controles
 {
-    public partial class ContactosView : UserControl
+    public partial class ContactosView : UserControl, IEnfocable
     {
+        public DelegateCommand AgregarContactoComando { get; }
+
+        public DelegateCommand BorrarContactoComando { get; }
+
         public ContactosView()
         {
+            AgregarContactoComando = new DelegateCommand(AgregarContacto);
+            BorrarContactoComando = new DelegateCommand(BorrarContacto);
+
             InitializeComponent();
-            Reestablecer();
+
+            EstiloIcono = (Style)FindResource("IconoLateral");
+
+            if (FindResource("Iconos") is CallbackConverter conversor)
+            {
+                conversor.Callback = (arg, param) =>
+               {
+                   FrameworkElement icono;
+                   if (arg is string clave && Iconos.ContainsKey(clave))
+                   {
+                       icono = Iconos[clave]();
+                   }
+                   else
+                   {
+                       icono = Iconos["Default"]();
+                   }
+
+                   if (param is "N")
+                       icono.Margin = new Thickness(16, 0, 0, 0);
+
+                   return icono;
+               };
+            }
+
+            var telefono = new TipoContacto
+            {
+                Nombre = "Teléfono",
+                ExpresionRegular = @"^( *\d){10} *$"
+            };
+
+            TiposContacto = new[] { telefono };
+
+            Contactos.Add(new Contacto
+            {
+                TipoContacto = telefono,
+                Informacion = "241 420 51 91"
+            });
         }
 
-        public void Reestablecer() => Contactos = new List<Contacto>();
+        private void Agregar_Click(object sender, RoutedEventArgs e) => OpcionesTiposContacto.IsOpen = true;
+
+        private async void AgregarContacto(object param)
+        {
+            if (param is TipoContacto contacto)
+            {
+                var nuevoContacto = new Contacto { TipoContacto = contacto };
+
+                Contactos.Add(nuevoContacto);
+
+                OpcionesTiposContacto.IsOpen = false;
+
+                await Task.Delay(200);
+                ObtenerCajasDeContactos().LastOrDefault()?.Focus();
+            }
+        }
+
+        private void BorrarContacto(object param)
+        {
+            if (param is Contacto c)
+                Contactos.Remove(c);
+        }
+
+        public void Enfocar()
+        {
+            if (Lista.HasItems)
+                ObtenerCajasDeContactos().FirstOrDefault()?.Focus();
+        }
+
+        private IEnumerable<TextBox> ObtenerCajasDeContactos() =>
+            Enumerable.Range(0, Lista.Items.Count)
+                .Select(Lista.ItemContainerGenerator.ContainerFromIndex)
+                .Where(i => i != null)
+                .Cast<ListViewItem>()
+                .Select(VisualTreeUtils.FindVisualChild<ContentPresenter>)
+                .Select(c => Lista.ItemTemplate.FindName("Box", c))
+                .Cast<TextBox>();
+
+        public static Style EstiloIcono { get; set; }
+
+        public Dictionary<string, Func<FrameworkElement>> Iconos { get; } = new Dictionary<string, Func<FrameworkElement>>
+        {
+            ["Default"] = () => new PackIconMaterial { Kind = PackIconMaterialKind.Contacts, Style = EstiloIcono },
+            ["Teléfono"] = () => new PackIconMaterial { Kind = PackIconMaterialKind.Phone, Style = EstiloIcono },
+            ["Email"] = () => new PackIconMaterial { Kind = PackIconMaterialKind.Email, Style = EstiloIcono }
+        };
 
         public IEnumerable<TipoContacto> TiposContacto
         {
@@ -23,16 +120,41 @@ namespace AguaSB.Usuarios.Views.Controles
             set { SetValue(TiposContactoProperty, value); }
         }
 
-        public IList<Contacto> Contactos
+        public ObservableCollection<Contacto> Contactos
         {
-            get { return (IList<Contacto>)GetValue(ContactosProperty); }
+            get { return (ObservableCollection<Contacto>)GetValue(ContactosProperty); }
             set { SetValue(ContactosProperty, value); }
         }
 
+        #region DP´s
         public static readonly DependencyProperty TiposContactoProperty =
             DependencyProperty.Register(nameof(TiposContacto), typeof(IEnumerable<TipoContacto>), typeof(ContactosView), new PropertyMetadata(Enumerable.Empty<TipoContacto>()));
 
         public static readonly DependencyProperty ContactosProperty =
-            DependencyProperty.Register(nameof(Contactos), typeof(IList<Contacto>), typeof(ContactosView), new PropertyMetadata(null));
+            DependencyProperty.Register(nameof(Contactos), typeof(ObservableCollection<Contacto>), typeof(ContactosView), new PropertyMetadata(new ObservableCollection<Contacto>()));
+        #endregion
+
+        #region Sincronizar icono de caja de texto
+        public static readonly DependencyProperty UnirConProperty =
+            DependencyProperty.RegisterAttached("UnirCon", typeof(TextBox), typeof(ContactosView), new PropertyMetadata(null, Unir));
+
+        private static void Unir(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is TextBox box && d is ContentControl elem && elem.Content is FrameworkElement contenido)
+            {
+                var b = new Binding
+                {
+                    Source = box,
+                    Path = new PropertyPath(nameof(IsKeyboardFocusWithin))
+                };
+
+                contenido.SetBinding(Icono.EnfocadoProperty, b);
+                contenido.SetValue(TagProperty, box);
+            }
+        }
+
+        public static void SetUnirCon(ContentControl elem, TextBox box) => elem.SetValue(UnirConProperty, box);
+        public static TextBox GetUnirCon(ContentControl elem) => (TextBox)elem.GetValue(UnirConProperty);
+        #endregion
     }
 }
--- AguaSB.Views/Utilerias/CallbackConverter.cs
+++ AguaSB.Views/Utilerias/CallbackConverter.cs
@@ -7,10 +7,10 @@ namespace AguaSB.Views.Utilerias
 {
     public class CallbackConverter : IValueConverter
     {
-        public Func<object, object> Callback { get; set; }
+        public Func<object, object, object> Callback { get; set; }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-            Callback?.Invoke(value) ?? DependencyProperty.UnsetValue;
+            Callback?.Invoke(value, parameter) ?? DependencyProperty.UnsetValue;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
             throw new InvalidOperationException();
