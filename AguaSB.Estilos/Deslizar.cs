3b47bb5816d16e3a2136bb9dfc3550a4016a646e
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-10 17:40:44 -0600

    Agregando deslizamiento suave a los deslizadores

---
+++ AguaSB.Estilos/Deslizar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace AguaSB.Estilos
+{
+    public static class Deslizar
+    {
+
+        public static void Aplicar(ScrollViewer panel, DoubleAnimation animacion) => new Deslizador(panel, animacion);
+
+        private class Deslizador : UIElement
+        {
+            public static readonly QuarticEase Balanceo = new QuarticEase { EasingMode = EasingMode.EaseOut };
+
+            public ScrollViewer Panel { get; }
+
+            public DoubleAnimation Animacion { get; }
+
+            public Deslizador(ScrollViewer panel, DoubleAnimation animacion)
+            {
+                Panel = panel ?? throw new ArgumentNullException(nameof(panel));
+                Animacion = animacion ?? throw new ArgumentNullException(nameof(animacion));
+
+                animacion.From = panel.VerticalOffset;
+                animacion.EasingFunction = Balanceo;
+
+                BeginAnimation(DesplazamientoProperty, animacion);
+            }
+
+            public double Desplazamiento
+            {
+                get { return (double)GetValue(DesplazamientoProperty); }
+                set { SetValue(DesplazamientoProperty, value); }
+            }
+
+            public static readonly DependencyProperty DesplazamientoProperty =
+                DependencyProperty.Register(nameof(Desplazamiento), typeof(double), typeof(Deslizador), new PropertyMetadata(0.0, Cambiado));
+
+            private static void Cambiado(DependencyObject d, DependencyPropertyChangedEventArgs e)
+            {
+                if (d is Deslizador deslizador && e.NewValue is double desplazamiento)
+                {
+                    deslizador.Panel.ScrollToVerticalOffset(desplazamiento);
+                }
+            }
+        }
+    }
+}
