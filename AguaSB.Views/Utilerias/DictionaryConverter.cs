1f92a201b90112e73fa1cb708845214a8375c12e
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-27 16:16:02 -0600

    Modificando conversor de Diccionario

--- AguaSB.Views/Utilerias/DictionaryConverter.cs
+++ AguaSB.Views/Utilerias/DictionaryConverter.cs
@@ -7,23 +7,42 @@ using System.Windows.Data;
 
 namespace AguaSB.Views.Utilerias
 {
+    public delegate object PostProcessCallback(object value, object parameter, object possibleValue);
+
     public class DictionaryConverter : IValueConverter
     {
         public IDictionary Dictionary { get; set; }
 
         public bool ThrowIfNotKeyFound { get; set; }
 
+        public object Default { get; set; }
+
+        public PostProcessCallback PostProcessCallback { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (Dictionary == null)
                 return DependencyProperty.UnsetValue;
 
+            object possibleValue = null;
+
             if (Dictionary.Contains(value))
-                return Dictionary[value];
+            {
+                possibleValue = Dictionary[value];
+            }
             else if (ThrowIfNotKeyFound)
+            {
                 throw new KeyNotFoundException($"The key {value} is not in the dictionary.");
+            }
             else
-                return DependencyProperty.UnsetValue;
+            {
+                if (Default != null)
+                    possibleValue = Default;
+                else if (Dictionary.Contains("Default"))
+                    possibleValue = Dictionary["Default"];
+            }
+
+            return (PostProcessCallback != null ? PostProcessCallback(value, parameter, possibleValue) : possibleValue) ?? DependencyProperty.UnsetValue;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
