fa16fb0fb08f0828e3e9e3e7b968afe3486f9f3c
ggzor <30713864+ggzor@users.noreply.github.com>
2017-11-27 16:15:16 -0600

    Agregando diccionarios de recursos de estilos

---
+++ AguaSB.Estilos/Diccionarios.cs
@@ -0,0 +1,17 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace AguaSB.Estilos
+{
+    public static class Diccionarios
+    {
+        public static readonly IDictionary<string, Func<FrameworkElement>> TiposContacto = new Dictionary<string, Func<FrameworkElement>>
+        {
+            ["Default"] = () => new PackIconMaterial { Kind = PackIconMaterialKind.Contacts },
+            ["Teléfono"] = () => new PackIconMaterial { Kind = PackIconMaterialKind.Phone },
+            ["Email"] = () => new PackIconMaterial { Kind = PackIconMaterialKind.Email }
+        };
+    }
+}
