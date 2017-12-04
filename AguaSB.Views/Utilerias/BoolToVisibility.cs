fedc8cc1c5cb49ece86a957f8665dee5886ddd5f
ggzor <30713864+ggzor@users.noreply.github.com>
2017-12-04 16:51:13 -0600

    Actualizando controles y conversores de la librería Views

---
+++ AguaSB.Views/Utilerias/BoolToVisibility.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AguaSB.Views.Utilerias
+{
+    public class BoolToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (bool?)(value);
+
+            switch ((string)parameter)
+            {
+                case "H":
+                    if (val == true)
+                        return Visibility.Visible;
+                    else
+                        return Visibility.Hidden;
+
+                case "HI":
+                case "IH":
+                    if (val == true)
+                        return Visibility.Hidden;
+                    else
+                        return Visibility.Visible;
+
+                case "I":
+                    if (val == true)
+                        return Visibility.Collapsed;
+                    else
+                        return Visibility.Visible;
+
+                case null:
+                case "C":
+                    if (val == true)
+                        return Visibility.Visible;
+                    else
+                        return Visibility.Collapsed;
+            }
+
+            throw new ArgumentException("The supplied parameter is not a valid parameter.", nameof(parameter));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new InvalidOperationException();
+    }
+}
---
+++ AguaSB.Views/Utilerias/DivideConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace AguaSB.Views.Utilerias
+{
+    public class DivideConverter : FuncValueConverter
+    {
+        public DivideConverter() : base(Dividir)
+        {
+        }
+
+        private static object Dividir(object v, object p)
+        {
+            if (double.TryParse(v?.ToString(), out double val) && double.TryParse(p?.ToString(), out double param) && param != 0.0)
+                return val / param;
+            else
+                return DependencyProperty.UnsetValue;
+        }
+    }
+}
