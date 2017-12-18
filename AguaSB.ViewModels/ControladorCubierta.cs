5e5470860ef61a1dad61a38c9c679d88f313d7de
ggzor <30713864+ggzor@users.noreply.github.com>
2017-12-18 13:36:41 -0600

    Agregando controlador de cubiertas

---
+++ AguaSB.ViewModels/ControladorCubierta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using AguaSB.Utilerias;
+
+namespace AguaSB.ViewModels
+{
+    public sealed class ControladorCubierta : Notificante
+    {
+        private bool mostrarCubierta;
+        private string textoCubierta;
+
+        public bool MostrarCubierta
+        {
+            get { return mostrarCubierta; }
+            set { N.Set(ref mostrarCubierta, value); }
+        }
+
+        public string TextoCubierta
+        {
+            get { return textoCubierta; }
+            set { N.Set(ref textoCubierta, value); }
+        }
+
+        public Cubierta Mostrar(string textoInicial = "") => new Cubierta(this, textoInicial);
+    }
+
+    public sealed class Cubierta : IDisposable
+    {
+        public ControladorCubierta Controlador { get; }
+
+        public Cubierta(ControladorCubierta controlador, string textoInicial)
+        {
+            Controlador = controlador ?? throw new ArgumentNullException(nameof(controlador));
+
+            Controlador.TextoCubierta = textoInicial;
+            Controlador.MostrarCubierta = true;
+        }
+
+        public string Texto
+        {
+            get { return Controlador.TextoCubierta; }
+            set { Controlador.TextoCubierta = value; }
+        }
+
+        public void Dispose()
+        {
+            Controlador.MostrarCubierta = false;
+        }
+    }
+}
