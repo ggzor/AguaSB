4a31a7809c6ff8cb2152a5e60c0e040d90e17761
ggzor <30713864+ggzor@users.noreply.github.com>
2017-12-20 17:00:06 -0600

    Agregando conversión de cantidad a letra

---
+++ AguaSB.Nucleo.Tests/Utilerias/CantidadALetraTests.cs
@@ -0,0 +1,125 @@
+﻿using NUnit.Framework;
+
+namespace AguaSB.Nucleo.Utilerias.Tests
+{
+    [TestFixture]
+    public class CantidadALetraTests
+    {
+        #region 0 - 9
+        [TestCase(0, "Cero pesos")]
+        [TestCase(1, "Un peso")]
+        [TestCase(2, "Dos pesos")]
+        [TestCase(3, "Tres pesos")]
+        [TestCase(4, "Cuatro pesos")]
+        [TestCase(5, "Cinco pesos")]
+        [TestCase(6, "Seis pesos")]
+        [TestCase(7, "Siete pesos")]
+        [TestCase(8, "Ocho pesos")]
+        [TestCase(9, "Nueve pesos")]
+        #endregion
+        public void CuandoNumeroEs_Menor10_ResultadoEs(int numero, string resultado) =>
+            RevisarResultadoEsIgualAConversion(numero, resultado);
+
+        #region 11 - 19
+        [TestCase(11, "Once pesos")]
+        [TestCase(12, "Doce pesos")]
+        [TestCase(13, "Trece pesos")]
+        [TestCase(14, "Catorce pesos")]
+        [TestCase(15, "Quince pesos")]
+        [TestCase(16, "Dieciséis pesos")]
+        [TestCase(17, "Diecisiete pesos")]
+        [TestCase(18, "Dieciocho pesos")]
+        [TestCase(19, "Diecinueve pesos")]
+        #endregion
+        public void CuandoNumeroEs_Menor20_ResultadoEs(int numero, string resultado) =>
+            RevisarResultadoEsIgualAConversion(numero, resultado);
+
+        #region 10 - 90
+        [TestCase(10, "Diez pesos")]
+        [TestCase(20, "Veinte pesos")]
+        [TestCase(30, "Treinta pesos")]
+        [TestCase(40, "Cuarenta pesos")]
+        [TestCase(50, "Cincuenta pesos")]
+        [TestCase(60, "Sesenta pesos")]
+        [TestCase(70, "Setenta pesos")]
+        [TestCase(80, "Ochenta pesos")]
+        [TestCase(90, "Noventa pesos")]
+        #endregion
+        public void CuandoNumeroEs_UnaDecena_ResultadoEs(int numero, string resultado) =>
+            RevisarResultadoEsIgualAConversion(numero, resultado);
+
+        #region 20 - 99
+        [TestCase(20, "Veinte pesos")]
+        [TestCase(25, "Veinte y cinco pesos")]
+        [TestCase(36, "Treinta y seis pesos")]
+        [TestCase(49, "Cuarenta y nueve pesos")]
+        [TestCase(60, "Sesenta pesos")]
+        [TestCase(64, "Sesenta y cuatro pesos")]
+        [TestCase(81, "Ochenta y un pesos")]
+        [TestCase(99, "Noventa y nueve pesos")]
+        #endregion
+        public void CuandoNumeroEs_Mayor20_Menor100_ResultadoEs(int numero, string resultado) =>
+            RevisarResultadoEsIgualAConversion(numero, resultado);
+
+        #region 100 - 900
+        [TestCase(100, "Cien pesos")]
+        [TestCase(200, "Doscientos pesos")]
+        [TestCase(300, "Trescientos pesos")]
+        [TestCase(400, "Cuatrocientos pesos")]
+        [TestCase(500, "Quinientos pesos")]
+        [TestCase(600, "Seiscientos pesos")]
+        [TestCase(700, "Setecientos pesos")]
+        [TestCase(800, "Ochocientos pesos")]
+        [TestCase(900, "Novecientos pesos")]
+        #endregion
+        public void CuandoNumeroEs_UnaCentena_ResultadoEs(int numero, string resultado) =>
+            RevisarResultadoEsIgualAConversion(numero, resultado);
+
+        #region 101 - 999
+        [TestCase(101, "Ciento un pesos")]
+        [TestCase(110, "Ciento diez pesos")]
+        [TestCase(111, "Ciento once pesos")]
+        [TestCase(121, "Ciento veinte y un pesos")]
+        [TestCase(120, "Ciento veinte pesos")]
+        [TestCase(144, "Ciento cuarenta y cuatro pesos")]
+        [TestCase(169, "Ciento sesenta y nueve pesos")]
+        [TestCase(240, "Doscientos cuarenta pesos")]
+        [TestCase(256, "Doscientos cincuenta y seis pesos")]
+        [TestCase(512, "Quinientos doce pesos")]
+        [TestCase(666, "Seiscientos sesenta y seis pesos")]
+        [TestCase(720, "Setecientos veinte pesos")]
+        [TestCase(999, "Novecientos noventa y nueve pesos")]
+        #endregion
+        public void CuandoNumeroEs_Mayor100_Menor1000_ResultadoEs(int numero, string resultado) =>
+            RevisarResultadoEsIgualAConversion(numero, resultado);
+
+        #region 1000 - 999,999
+        [TestCase(1_000, "Un mil pesos")]
+        [TestCase(1_111, "Un mil ciento once pesos")]
+        [TestCase(1_500, "Un mil quinientos pesos")]
+        [TestCase(2_000, "Dos mil pesos")]
+        [TestCase(2_500, "Dos mil quinientos pesos")]
+        [TestCase(5_100, "Cinco mil cien pesos")]
+        [TestCase(10_000, "Diez mil pesos")]
+        [TestCase(15_000, "Quince mil pesos")]
+        [TestCase(100_000, "Cien mil pesos")]
+        [TestCase(666_666, "Seiscientos sesenta y seis mil seiscientos sesenta y seis pesos")]
+        [TestCase(999_999, "Novecientos noventa y nueve mil novecientos noventa y nueve pesos")]
+        #endregion
+        public void CuandoNumeroEs_Mayor1000_MenorMillon_ResultadoEs(int numero, string resultado) =>
+            RevisarResultadoEsIgualAConversion(numero, resultado);
+
+        #region 1,000,000 - 999,999,999
+        [TestCase(1_000_000, "Un millón de pesos")]
+        [TestCase(1_234_567, "Un millón doscientos treinta y cuatro mil quinientos sesenta y siete pesos")]
+        [TestCase(611_510_000, "Seiscientos once millones quinientos diez mil pesos")]
+        [TestCase(900_000_000, "Novecientos millones de pesos")]
+        [TestCase(999_999_999, "Novecientos noventa y nueve millones novecientos noventa y nueve mil novecientos noventa y nueve pesos")]
+        #endregion
+        public void CuandoNumeroEs_MayorMillon_Menor1000Millones_ResultadoEs(int numero, string resultado) =>
+            RevisarResultadoEsIgualAConversion(numero, resultado);
+
+        private void RevisarResultadoEsIgualAConversion(int numero, string resultado) =>
+            Assert.AreEqual(resultado, CantidadALetra.Convertir(numero));
+    }
+}
---
+++ AguaSB.Nucleo/Utilerias/CantidadALetra.cs
@@ -0,0 +1,127 @@
+﻿using AguaSB.Utilerias;
+using MoreLinq;
+using System.Linq;
+
+namespace AguaSB.Nucleo.Utilerias
+{
+    public static class CantidadALetra
+    {
+        private const string NombresUnidades = "un,dos,tres,cuatro,cinco,seis,siete,ocho,nueve";
+        private const string NombresMenores20 = "once,doce,trece,catorce,quince,dieciséis,diecisiete,dieciocho,diecinueve";
+        private const string NombresDecenas = "diez,veinte,treinta,cuarenta,cincuenta,sesenta,setenta,ochenta,noventa";
+        private const string NombresCentenas = "ciento,doscientos,trescientos,cuatrocientos,quinientos,seiscientos,setecientos,ochocientos,novecientos";
+
+
+        private static string[] ConvertirValoresArreglo(string valores) =>
+            valores.Split(',')
+            .Select(s => s.Trim())
+            .Prepend("")
+            .ToArray();
+
+        private static readonly string[] Unidades = ConvertirValoresArreglo(NombresUnidades);
+        private static readonly string[] Menores20 = ConvertirValoresArreglo(NombresMenores20);
+        private static readonly string[] Decenas = ConvertirValoresArreglo(NombresDecenas);
+        private static readonly string[] Centenas = ConvertirValoresArreglo(NombresCentenas);
+
+        public static string Convertir(int numero)
+        {
+            if (numero == 0)
+                return "Cero pesos";
+            if (numero == 1)
+                return "Un peso";
+
+            string resultado = ConvertirMenorMilMillones(numero);
+
+            return $"{resultado} pesos".Capitalizar();
+        }
+
+        private const int UnMillon = 1_000_000;
+        private static string ConvertirMenorMilMillones(int numero)
+        {
+            if (numero < UnMillon)
+                return ConvertirMenorUnMillon(numero);
+
+            var millones = numero / UnMillon;
+            var parteMenorMillon = numero % UnMillon;
+
+            string resultado;
+
+            if (millones == 1)
+                resultado = "Un millón";
+            else
+                resultado = $"{ConvertirMenorMil(millones)} millones";
+
+            if (parteMenorMillon == 0)
+                resultado += " de";
+            else
+                resultado += $" {ConvertirMenorUnMillon(parteMenorMillon)}";
+
+            return resultado;
+        }
+
+        private const int Mil = 1000;
+        private static string ConvertirMenorUnMillon(int numero)
+        {
+            if (numero < Mil)
+                return ConvertirMenorMil(numero);
+
+            var millares = numero / Mil;
+            var parteMenorMil = numero % Mil;
+
+            var resultado = $"{ConvertirMenorMil(millares)} mil";
+
+            if (parteMenorMil != 0)
+                resultado += $" {ConvertirMenorMil(parteMenorMil)}";
+
+            return resultado;
+        }
+
+        private const int Cien = 100;
+
+        private static string ConvertirMenorMil(int numero)
+        {
+            if (numero < Cien)
+                return ConvertirMenorCien(numero);
+
+            if (numero == Cien)
+                return "cien";
+
+            var centena = numero / Cien;
+            var decenaYUnidad = numero % Cien;
+
+            var resultado = Centenas[centena];
+
+            if (decenaYUnidad != 0)
+                resultado += $" {ConvertirMenorCien(decenaYUnidad)}";
+
+            return resultado;
+        }
+
+        private static string ConvertirMenorCien(int numero)
+        {
+            if (numero < 20)
+                return ConvertirMenorVeinte(numero);
+
+            var decena = numero / 10;
+            var unidad = numero % 10;
+
+            var resultado = Decenas[decena];
+
+            if (unidad != 0)
+                resultado += $" y {Unidades[unidad]}";
+
+            return resultado;
+        }
+
+
+        private static string ConvertirMenorVeinte(int numero)
+        {
+            if (numero < 10)
+                return Unidades[numero];
+            else if (numero == 10)
+                return "diez";
+            else
+                return Menores20[numero - 10];
+        }
+    }
+}
