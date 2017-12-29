6eae4dc5ae10406123f4feea7c0459f0bafdb143
ggzor <30713864+ggzor@users.noreply.github.com>
2017-12-29 13:47:05 -0600

    Agregando adición automática de usuarios

--- AguaSB/Inicializadores/AgregarTiposContrato.cs
+++ AguaSB/Inicializadores/AgregarTiposContrato.cs
@@ -10,30 +10,30 @@ namespace AguaSB.Inicializadores
 {
     public class AgregarTiposContrato : IInicializador
     {
+        private static readonly TipoContrato MedioServicio = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 0.5m, Nombre = "Medio servicio", Clave = "AB" };
+        private static readonly TipoContrato Baja = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 0.0m, Nombre = "Baja", Clave = "BJ" };
+
+        public static readonly IReadOnlyDictionary<string, TipoContrato> TiposContrato = new Dictionary<string, TipoContrato>
+        {
+            ["A"] = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 1.0m, Nombre = "Normal", Clave = "A" },
+            ["A1"] = MedioServicio,
+            ["AB"] = MedioServicio,
+            ["B"] = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 0.5m, Nombre = "Tercera edad", Clave = "B" },
+            ["B1"] = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 0.25m, Nombre = "Persona mayor", Clave = "B1" },
+            ["BT"] = Baja,
+            ["BD"] = Baja,
+            ["C"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 2.0m, Nombre = "Normal", Clave = "C" },
+            ["C1"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 3.0m, Nombre = "Extra", Clave = "C1" },
+            ["C2"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 8.5m, Nombre = "Hotel", Clave = "C2" },
+            ["C3"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 3.0m, Nombre = "Hotel Rigel", Clave = "C3" },
+            ["C4"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 5.0m, Nombre = "Hotel Chimalpa", Clave = "C4" },
+            ["C5"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 4.0m, Nombre = "Doble", Clave = "C5" },
+            ["D"] = new TipoContrato { ClaseContrato = ClaseContrato.Industrial, Multiplicador = 8m, Nombre = "Normal", Clave = "D" },
+            ["D2"] = new TipoContrato { ClaseContrato = ClaseContrato.Industrial, Multiplicador = 8.5m, Nombre = "Blockera", Clave = "D2" }
+        };
+
         public AgregarTiposContrato(IDbContextScopeFactory ambito, IRepositorio<TipoContrato> tiposContratoRepo)
         {
-            var medioServicio = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 0.5m, Nombre = "Medio servicio", Clave = "AB" };
-            var baja = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 0.0m, Nombre = "Baja", Clave = "BJ" };
-
-            var tiposContrato = new Dictionary<string, TipoContrato>
-            {
-                ["A"] = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 1.0m, Nombre = "Normal", Clave = "A" },
-                ["A1"] = medioServicio,
-                ["AB"] = medioServicio,
-                ["B"] = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 0.5m, Nombre = "Tercera edad", Clave = "B" },
-                ["B1"] = new TipoContrato { ClaseContrato = ClaseContrato.Doméstico, Multiplicador = 0.25m, Nombre = "Persona mayor", Clave = "B1" },
-                ["BT"] = baja,
-                ["BD"] = baja,
-                ["C"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 2.0m, Nombre = "Normal", Clave = "C" },
-                ["C1"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 3.0m, Nombre = "Extra", Clave = "C1" },
-                ["C2"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 8.5m, Nombre = "Hotel", Clave = "C2" },
-                ["C3"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 3.0m, Nombre = "Hotel Rigel", Clave = "C3" },
-                ["C4"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 5.0m, Nombre = "Hotel Chimalpa", Clave = "C4" },
-                ["C5"] = new TipoContrato { ClaseContrato = ClaseContrato.Comercial, Multiplicador = 4.0m, Nombre = "Doble", Clave = "C5" },
-                ["D"] = new TipoContrato { ClaseContrato = ClaseContrato.Industrial, Multiplicador = 8m, Nombre = "Normal", Clave = "D" },
-                ["D2"] = new TipoContrato { ClaseContrato = ClaseContrato.Industrial, Multiplicador = 8.5m, Nombre = "Blockera", Clave = "D2" }
-            };
-
             using (var baseDeDatos = ambito.Create())
             {
                 Console.WriteLine("Agregando tipos de contrato...");
@@ -44,7 +44,7 @@ namespace AguaSB.Inicializadores
                     return;
                 }
 
-                tiposContrato.Values.Distinct().ForEach(t => tiposContratoRepo.Agregar(t));
+                TiposContrato.Values.Distinct().ForEach(t => tiposContratoRepo.Agregar(t));
                 baseDeDatos.SaveChanges();
             }
         }
---
+++ AguaSB/Inicializadores/AgregarUsuarios.cs
@@ -0,0 +1,143 @@
+﻿using AguaSB.Datos;
+using AguaSB.Nucleo;
+using AguaSB.Utilerias;
+using Mehdime.Entity;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AguaSB.Inicializadores
+{
+    public class AgregarUsuarios : IInicializador
+    {
+        public AgregarUsuarios(IDbContextScopeFactory ambito, IRepositorio<Usuario> usuariosRepo, IRepositorio<TipoContrato> tiposContratoRepo, IRepositorio<Nota> notasRepo)
+        {
+            var parejasNotas = new LinkedList<(Contrato Contrato, int Fila)>();
+
+            using (var baseDeDatos = ambito.Create())
+            {
+                Console.WriteLine("Registrando usuarios....");
+
+                if (usuariosRepo.Datos.Any())
+                {
+                    Console.WriteLine("Ya se han registrado los usuarios.");
+                    return;
+                }
+
+                var tiposContrato = tiposContratoRepo.Datos.ToDictionary(t => t.Clave);
+
+                var usuarios = LeerUsuarios();
+                Console.WriteLine($"Se registrarán {usuarios.Count} usuarios.");
+
+                var secciones = (from valor in new[] { "", "Primera", "Segunda", "Tercera", "Cuarta" }.Index()
+                                 select new Seccion { Nombre = valor.Value, Orden = valor.Key })
+                                .ToArray();
+
+                var calles = (from u in usuarios
+                              let calle = u.Contratos.Single().Domicilio.Calle
+                              select (Seccion: calle.Seccion.Orden, Nombre: calle.Nombre.Trim()))
+                             .Distinct()
+                             .Select(t => new Calle { Nombre = t.Nombre, Seccion = secciones[t.Seccion] })
+                             .ToArray();
+
+                usuarios = NormalizarDuplicados(usuarios);
+
+                foreach (Persona persona in usuarios)
+                {
+                    var usuario = new Persona
+                    {
+                        Nombre = persona.Nombre,
+                        ApellidoPaterno = persona.ApellidoPaterno,
+                        ApellidoMaterno = persona.ApellidoMaterno,
+                        FechaRegistro = Fecha.Ahora
+                    };
+
+                    foreach (var c in persona.Contratos)
+                    {
+                        var contrato = new Contrato
+                        {
+                            TipoContrato = tiposContrato[AgregarTiposContrato.TiposContrato[c.TipoContrato.Nombre].Clave],
+                            Domicilio = new Domicilio
+                            {
+                                Numero = c.Domicilio.Numero.Take(20).ToDelimitedString(""),
+                                Calle = calles.Single(calle => calle.Seccion.Orden == c.Domicilio.Calle.Seccion.Orden && calle.Nombre == c.Domicilio.Calle.Nombre),
+                            },
+                            FechaRegistro = Fecha.Ahora,
+                            MedidaToma = c.MedidaToma,
+                        };
+
+                        var p = c.Pagos.Single();
+
+                        var pago = new Pago
+                        {
+                            CantidadPagada = 0,
+                            Monto = 0,
+                            Desde = p.Desde,
+                            Hasta = p.Hasta,
+                            FechaPago = p.FechaPago,
+                            FechaRegistro = Fecha.Ahora,
+                        };
+
+                        contrato.Pagos.Add(pago);
+                        usuario.Contratos.Add(contrato);
+
+                        parejasNotas.AddLast((contrato, c.Domicilio.Id));
+                    }
+                    usuariosRepo.Agregar(usuario);
+                }
+                baseDeDatos.SaveChanges();
+                Console.WriteLine("Listo.");
+            }
+
+            if (parejasNotas.Any())
+            {
+                using (var baseDeDatos = ambito.Create())
+                {
+                    Console.WriteLine("Registrando notas de referencias...");
+
+                    var tipoNota = new TipoNota { FechaRegistro = Fecha.Ahora, Nombre = "_Contrato_Fila" };
+
+                    parejasNotas.Select(p => new Nota
+                    {
+                        FechaRegistro = Fecha.Ahora,
+                        Informacion = p.Fila.ToString(),
+                        Referencia = p.Contrato.Id,
+                        Tipo = tipoNota
+                    }).ForEach(n => notasRepo.Agregar(n));
+
+                    baseDeDatos.SaveChanges();
+                    Console.WriteLine("Listo.");
+                }
+            }
+        }
+
+        private IReadOnlyList<Usuario> NormalizarDuplicados(IReadOnlyList<Usuario> usuarios)
+        {
+            var agrupados = usuarios.GroupBy(u => u.NombreCompleto);
+
+            var contratoUnico = agrupados.Where(g => g.Count() == 1).Select(g => g.Single());
+
+            var contratoMultiple = agrupados.Where(g => g.Count() > 1).Select(g => g.ToArray()).Select(g =>
+            {
+                var primero = g[0];
+
+                g.Skip(1).ForEach(u => primero.Contratos.Add(u.Contratos.Single()));
+
+                return primero;
+            });
+
+            return contratoUnico.Concat(contratoMultiple).OrderBy(u => u.Id).ToList();
+        }
+
+        private IReadOnlyList<Usuario> LeerUsuarios()
+        {
+            var archivo = new FileInfo("Usuarios.json");
+
+            using (var flujo = new StreamReader(archivo.OpenRead()))
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<List<Persona>>(flujo.ReadToEnd());
+        }
+
+    }
+}
