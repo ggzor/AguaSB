62ee8f2b17b50e16144becda34c04f60a6b7a683
ggzor <30713864+ggzor@users.noreply.github.com>
2018-01-02 15:16:00 -0600

    Permitiendo cambio de colores de montos

--- AguaSB.Views.Nucleo/DetallesPagoView.xaml.cs
+++ AguaSB.Views.Nucleo/DetallesPagoView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace AguaSB.Views.Nucleo
 {
@@ -21,5 +22,14 @@ namespace AguaSB.Views.Nucleo
 
         public static readonly DependencyProperty DetallesPagoProperty =
             DependencyProperty.Register(nameof(DetallesPago), typeof(IEnumerable<IDetallePago>), typeof(DetallesPagoView), new PropertyMetadata(Enumerable.Empty<IDetallePago>()));
+
+        public Brush ColorMontos
+        {
+            get { return (Brush)GetValue(ColorMontosProperty); }
+            set { SetValue(ColorMontosProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorMontosProperty =
+            DependencyProperty.Register(nameof(ColorMontos), typeof(Brush), typeof(DetallesPagoView), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0x4C, 0xAF, 0x50))));
     }
 }
