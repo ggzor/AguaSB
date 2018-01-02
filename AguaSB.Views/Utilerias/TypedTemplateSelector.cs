8ebf279d866e17efb45d45934bde8f3df529ae21
ggzor <30713864+ggzor@users.noreply.github.com>
2018-01-02 10:39:28 -0600

    Agregando selector de plantillas por tipo

---
+++ AguaSB.Views/Utilerias/TypedTemplateSelector.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AguaSB.Views.Utilerias
+{
+    class TypedTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) =>
+            (DataTemplate)(((FrameworkElement)container).FindResource(item.GetType()));
+    }
+}
