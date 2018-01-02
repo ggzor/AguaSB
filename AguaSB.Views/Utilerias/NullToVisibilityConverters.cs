8c4f3dbd71668b72c6ca743062480e3d05fde074
ggzor <30713864+ggzor@users.noreply.github.com>
2018-01-02 15:04:46 -0600

    Agregando conversores de null  a visibilidad

---
+++ AguaSB.Views/Utilerias/NullToVisibilityConverters.cs
@@ -0,0 +1,20 @@
+﻿namespace AguaSB.Views.Utilerias
+{
+    public class IsNullToVisibilityConverter : FuncValueConverter
+    {
+        private static readonly BoolToVisibility B2V = new BoolToVisibility();
+
+        public IsNullToVisibilityConverter() : base(o => B2V.Convert(o == null, null, null, null))
+        {
+        }
+    }
+
+    public class IsNotNullToVisibilityConverter : FuncValueConverter
+    {
+        private static readonly BoolToVisibility B2V = new BoolToVisibility();
+
+        public IsNotNullToVisibilityConverter() : base(o => B2V.Convert(o != null, null, null, null))
+        {
+        }
+    }
+}
