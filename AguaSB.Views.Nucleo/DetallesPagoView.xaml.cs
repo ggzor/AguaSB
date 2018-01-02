5c8f9d5db3a06c1fbb920b9634215a895582be57
ggzor <30713864+ggzor@users.noreply.github.com>
2018-01-02 11:10:27 -0600

    Extrayendo funcionalidad de vista de detalles de pago

---
+++ AguaSB.Views.Nucleo/DetallesPagoView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using AguaSB.Nucleo.Pagos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AguaSB.Views.Nucleo
+{
+    public partial class DetallesPagoView : UserControl
+    {
+        public DetallesPagoView()
+        {
+            InitializeComponent();
+        }
+
+        public IEnumerable<IDetallePago> DetallesPago
+        {
+            get { return (IEnumerable<IDetallePago>)GetValue(DetallesPagoProperty); }
+            set { SetValue(DetallesPagoProperty, value); }
+        }
+
+        public static readonly DependencyProperty DetallesPagoProperty =
+            DependencyProperty.Register(nameof(DetallesPago), typeof(IEnumerable<IDetallePago>), typeof(DetallesPagoView), new PropertyMetadata(Enumerable.Empty<IDetallePago>()));
+    }
+}
