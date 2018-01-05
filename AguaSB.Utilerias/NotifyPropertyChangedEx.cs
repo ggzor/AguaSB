1d314c95db57b2692f7b52df7b7c05b06b157eef
ggzor <30713864+ggzor@users.noreply.github.com>
2018-01-05 13:39:49 -0600

    Agregando utilería para el manejo de propiedades observables con tipeado estático

--- AguaSB.Utilerias/NotifyPropertyChangedEx.cs
+++ AguaSB.Utilerias/NotifyPropertyChangedEx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reactive.Linq;
 
 namespace AguaSB.Utilerias
@@ -16,6 +17,25 @@ namespace AguaSB.Utilerias
                 handler => notifier.PropertyChanged += handler,
                 handler => notifier.PropertyChanged -= handler)
                 .Select(eventPattern => (eventPattern.Sender, eventPattern.EventArgs));
+        }
+
+        public static IObservable<TProp> ObservableProperty<TObj, TProp>(this TObj obj, Expression<Func<TObj, TProp>> expression) where TObj : INotifyPropertyChanged
+        {
+            if (expression.NodeType != ExpressionType.Lambda)
+                throw new ArgumentException("The expression must be a Lambda.");
+
+            if (expression.Body is MemberExpression expr && expr.NodeType == ExpressionType.MemberAccess)
+            {
+                var func = expression.Compile();
+
+                return from e in obj.ToObservableProperties()
+                       where e.Args.PropertyName == expr.Member.Name
+                       select func(obj);
+            }
+            else
+            {
+                throw new ArgumentException("The expression must be a member access.");
+            }
         }
     }
 }
