48186a9c9ddf64aa7e515cfa053355f9e7d2834e
ggzor <30713864+ggzor@users.noreply.github.com>
2018-01-18 11:05:05 -0600

    Agregando conversores adicionales

---
+++ AguaSB.Views/Utilerias/EqualsToConverters.cs
@@ -0,0 +1,23 @@
+﻿namespace AguaSB.Views.Utilerias
+{
+    internal static class Stringify
+    {
+        public static string String(this object o) => o?.ToString() ?? "null";
+    }
+
+    public class EqualsToConverter : FuncValueConverter
+    {
+        public EqualsToConverter() : base(Convertir) { }
+
+        private static object Convertir(object valor, object parametro) => valor.String() == parametro.String();
+    }
+
+    public class NotEqualsToConverter : FuncValueConverter
+    {
+        public NotEqualsToConverter() : base(Convertir)
+        {
+        }
+
+        private static object Convertir(object valor, object parametro) => valor.String() == parametro.String();
+    }
+}
---
+++ AguaSB.Views/Utilerias/ZeroConverters.cs
@@ -0,0 +1,16 @@
+﻿namespace AguaSB.Views.Utilerias
+{
+    public class IsZeroConverter : FuncValueConverter
+    {
+        public IsZeroConverter() : base(o => (o as decimal?) == 0.0M)
+        {
+        }
+    }
+
+    public class IsNotZeroConverter : FuncValueConverter
+    {
+        public IsNotZeroConverter() : base(o => ((o as decimal?) ?? -1.0M) != 0.0M)
+        {
+        }
+    }
+}
