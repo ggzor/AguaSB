2a15846b4cb5e0d32e9b33ac44c7c5faac550643
ggzor <30713864+ggzor@users.noreply.github.com>
2018-01-20 21:08:44 -0600

    Agregando conversores adicionales

---
+++ AguaSB.Views/Utilerias/ZeroToVisibilityConverters.cs
@@ -0,0 +1,23 @@
+﻿namespace AguaSB.Views.Utilerias
+{
+    internal static class Util
+    {
+        public static readonly IsZeroConverter IsZeroConverter = new IsZeroConverter();
+        public static readonly IsNotZeroConverter IsNotZeroConverter = new IsNotZeroConverter();
+        public static readonly BoolToVisibility BoolToVisibility = new BoolToVisibility();
+    }
+
+    public class IsZeroToVisibilityConverter : FuncValueConverter
+    {
+        public IsZeroToVisibilityConverter() : base((o, p) => Util.BoolToVisibility.Convert(Util.IsZeroConverter.Convert(o, null, null, null), null, p, null))
+        {
+        }
+    }
+
+    public class IsNotZeroToVisibilityConverter : FuncValueConverter
+    {
+        public IsNotZeroToVisibilityConverter() : base((o, p) => Util.BoolToVisibility.Convert(Util.IsNotZeroConverter.Convert(o, null, null, null), null, p, null))
+        {
+        }
+    }
+}
