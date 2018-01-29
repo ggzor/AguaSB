6b60bbbac54f74f30ecf21d3b7e2e9bdb2f8e2bb
ggzor <30713864+ggzor@users.noreply.github.com>
2018-01-28 20:10:52 -0600

    Agregando nueva interfaz de pago

---
+++ AguaSB.Pagos.ViewModels/Pagar.cs
@@ -0,0 +1,15 @@
+﻿using AguaSB.ViewModels;
+using AguaSB.Navegacion;
+
+namespace AguaSB.Pagos.ViewModels
+{
+    public class Pagar : IViewModel
+    {
+        public INodo Nodo { get; }
+
+        public Pagar()
+        {
+            Nodo = new Nodo { };
+        }
+    }
+}
---
+++ AguaSB.Pagos.Views/Pagar.xaml.cs
@@ -0,0 +1,17 @@
+﻿using AguaSB.Views;
+using System;
+using System.Windows.Controls;
+
+namespace AguaSB.Pagos.Views
+{
+    public partial class Pagar : UserControl, IView
+    {
+        public ViewModels.Pagar ViewModel { get; }
+
+        public Pagar(ViewModels.Pagar pagar)
+        {
+            DataContext = ViewModel = pagar ?? throw new ArgumentNullException(nameof(pagar));
+            InitializeComponent();
+        }
+    }
+}
